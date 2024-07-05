9e14c8657f9dd8c9e8c666427f3e3ca29a9bdcb6
Samuel_Prog_CSec <samuel.blaper@gmail.com>
2024-07-05 09:11:16 +0200

    chore: Refactor NMap_Busqueda_Puertos_LINUX.py for improved readability and maintainability

--- NMap_Busqueda_Puertos_LINUX.py
+++ NMap_Busqueda_Puertos_LINUX.py
@@ -3,32 +3,46 @@ import sys
 import re
 
 # Puertos comunes para escanear en una red con -PS y -PA
-ports = [21, 22, 23, 25, 53, 80, 110, 111, 135, 139, 143, 443, 445, 
+common_ports = [21, 22, 23, 25, 53, 80, 110, 111, 135, 139, 143, 443, 445, 
          993, 995, 1723, 3306, 3389, 5900, 8080]
 
-def run_nmap(ip_cidr):
-    # Comando Nmap básico para escanear puertos y servicios
+def run_nmap(ip):
+    # Convertir la lista de puertos a una cadena separada por comas
+    ports_str = ",".join(map(str, common_ports))
+    
+    # Comando Nmap para escanear puertos y servicios
     command = [
         "sudo", "nmap", "-sS", "-T3", "-p-", "-sV", "--version-intensity", "1",
         "-Pn", "-v", "--randomize-hosts", "-f", "-D", "RND:10",
-        "-PS80,443,22", "-PA80,443,22", ip_cidr
+        f"-PS{ports_str}", f"-PA{ports_str}", ip
     ]
     
-    try:
-        # Ejecutar el comando y capturar la salida
-        result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        output = result.stdout
-        print(output)
+    # Prueba ejecución de comando
+    func_aux_command(command)
+    
+    # try:
+    #     # Ejecutar el comando y capturar la salida
+    #     result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    #     output = result.stdout
+    #     print(output)
         
-        if result.stderr:
-            print(f"Error: {result.stderr}", file=sys.stderr)
+    #     if result.stderr:
+    #         print(f"Error: {result.stderr}", file=sys.stderr)
 
-        # Analizar la salida para obtener TTL, tamaño de ventana y realizar banner grabbing
-        determine_os(output)
-        banner_grabbing(output, ip_cidr)
+    #     # Analizar la salida para obtener TTL, tamaño de ventana y realizar banner grabbing
+    #     determine_os(output)
+    #     banner_grabbing(output, ip_cidr)
         
-    except Exception as e:
-        print(f"Exception occurred: {e}", file=sys.stderr)
+    # except Exception as e:
+    #     print(f"Exception occurred: {e}", file=sys.stderr)
+        
+def func_aux_command(command):
+    # Construir el comando como una cadena de texto
+    command_str = " ".join(command)
+    
+    # Imprimir el comando
+    print(command_str)
+    
 
 def determine_os(nmap_output):
     # Patrón para capturar TTL y tamaño de ventana
@@ -92,12 +106,48 @@ def banner_grabbing(nmap_output, ip_cidr):
             if result.stderr:
                 print(f"Error: {result.stderr}", file=sys.stderr)
         except Exception as e:
-            print(f"Exception occurred: {e}", file=sys.stderr)
+            print(f"Exception occurred: {e}", file=sys.stderr)     
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python nmap_scan.py <IP/CIDR>")
+# Función para validar la entrada del usuario
+def validate_parameter(entrada):
+    # Verificar si la entrada una dir. IP de red con CIDR
+    if '/' in entrada:
+        ip, cidr = entrada.split('/')
+        if validate_ip(ip) and (cidr.isdigit() and 0 <= int(cidr) <= 32):
+            return "cidr", entrada
+        else: 
+            return "incorrect_format", None
+    else:
+        # Verificar si la entrada es una dirección IP de un host
+        if validate_ip(entrada):
+            return "ip", entrada
+        else:
+            return "incorrect_format", None
+    
+def validate_ip(ip):
+    # Validar que la IP tenga 4 octetos y cada uno sea un número entre 0 y 255
+    octetos = ip.split('.')
+    if len(octetos) != 4:
+        return False
+    for octeto in octetos:
+        if not octeto.isdigit() or not 0 <= int(octeto) <= 255:
+            return False
+    return True
+
+if __name__ == "__main__":    
+    entrada_usuario = input("Ingrese una dirección IP o red con CIDR: ")
+    tipo_entrada, ip = validate_parameter(entrada_usuario)
+    
+    if tipo_entrada == "cidr":
+        print(f"Procesando red CIDR: {entrada_usuario}")
+        # Llamar a la función para escanear la red CIDR
+        run_nmap(ip)
+        
+    elif tipo_entrada == "ip":
+        print(f"Procesando dirección IP: {entrada_usuario}")
+        # Llamar a la función para escanear la dirección IP
+        run_nmap(ip)
+        
+    else:
+        print("La entrada es incorrecta. Por favor, ingrese una dirección IP válida o una red con CIDR.")
         sys.exit(1)
-
-    ip_cidr = sys.argv[1]
-    run_nmap(ip_cidr)
